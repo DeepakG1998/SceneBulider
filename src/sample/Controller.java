d8704704bcd864f2b0879fd88ac97dd7c80e19e6
DeepakG1998 <74721977+DeepakG1998@users.noreply.github.com>
2021-02-11 20:41:29 +0530

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,27 @@
+package sample;
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+public class Controller {
+public TextArea text;
+public void pressButton(ActionEvent event) throws IOException {
+  {
+   String s=text.getText();
+
+  File f=new File("E:\\Deepak423.txt");
+  FileWriter fw=new FileWriter(f);
+   fw.write(s);
+  fw.close();
+ }
+
+ }
+ void textArea(ActionEvent event)
+ {
+
+ }
+
+}
+
